f2d4fbd6c18e159977951208dbf6f5fe4231c80e
myke-roly <myke_roly@hotmail.com>
2020-12-13 06:30:43 -0300

    initial commit

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,8 +1,22 @@
-import express ,{ Express } from 'express';
+import express, { Express, Response } from 'express';
+import dotenv from 'dotenv';
 
+dotenv.config();
 const app: Express = express();
-const PORT: string | number = process.env.PORT || 3030;
+const PORT: string | number = process.env.PORT || 8080;
+
+app.get('/', (_, res: Response) => {
+  res.json({
+    message: 'starting',
+  });
+});
+
+app.get('/test', (_, res: Response) => {
+  res.json({
+    message: 'testing another routes',
+  });
+});
 
 app.listen(PORT, () => {
   console.log('Listen on port: ' + PORT);
-})
+});
